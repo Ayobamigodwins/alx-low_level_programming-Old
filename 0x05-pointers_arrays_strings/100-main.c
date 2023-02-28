bc4455aa5f5c4d2c6dbdceea50c4136f8bd5fde4
Ayobamigodwins <ayobamigodwins@gmail.com>
2023-02-27 17:03:21 -0800

    Task 10 and 11

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int nb;
+
+    nb = atoi("98");
+    printf("%d\n", nb);
+    nb = atoi("-402");
+    printf("%d\n", nb);
+    nb = atoi("-98");
+    printf("%d\n", nb);
+    nb = atoi("214748364");
+    printf("%d\n", nb);
+    nb = atoi("0");
+    printf("%d\n", nb);
+    nb = atoi("402");
+    printf("%d\n", nb);
+    nb = atoi("98");
+    printf("%d\n", nb);
+    nb = atoi("402");
+    printf("%d\n", nb);
+    return (0);
+}
