4c1a26da655545d3608b82d28acb148dd508288e
Ayobamigodwins <ayobamigodwins@gmail.com>
2023-03-14 16:07:59 -0700

    Task 0 Correction 11

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *s;
+	char *a = malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (!size || a == NULL)
 		return (NULL);
-
-	s = malloc(size * sizeof(*s));
-
-	for (i = 0; i < size; i++)
-		s[i] = c;
-
-	return (s);
+	while (size--)
+		a[size] = c;
+	return (a);
 }
